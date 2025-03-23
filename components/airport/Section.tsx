3bca13d530782857ef71cb56719c2afbd3b3e6c4
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-03-23 18:56:56 -0500

    feat(section): create Section component

---
+++ components/airport/Section.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+import Image from 'next/image';
+
+interface SectionProps {
+  icon: string;
+  sectionTitle: string;
+}
+
+export const Section: FC<SectionProps> = ({
+  icon,
+  sectionTitle,
+}) => {
+  return (
+    <div className="flex flex-row items-center gap-5">
+      <div className='w-fit'>
+        <Image
+          className="dark:invert"
+          src={icon}
+          alt="Info logo"
+          width={35}
+          height={35}
+          priority
+        />
+      </div>
+      <div className='w-fit'>
+        <h3 className="font-medium text-[2.5rem] bg-gradient-to-r from-[#006AFF] to-[#00F9FF] bg-clip-text text-transparent">{sectionTitle}</h3>
+      </div>
+    </div>
+  );
+};
