29fff1ba74eae42a011c362acf3b243852b9aba3
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-03-24 04:52:37 -0500

    feat(location-page): integrate map and show dynamic coordinates

--- app/airport/[iata_code]/location/page.tsx
+++ app/airport/[iata_code]/location/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useAirportStore } from '@/store/airportStore';
 import AirportLocation from '@/components/airport/AirportLocation';
+import AirportMap from '@/components/airport/AirportMap';
 
 export default function Page() {
   const { currentAirport } = useAirportStore();
@@ -9,8 +10,9 @@ export default function Page() {
   }
   const { latitude, longitude, geoname_id } = currentAirport
   return (
-    <div>
+    <div className="flex flex-col gap-[1.875rem]">
       <AirportLocation latitude={latitude} longitude={longitude} geoname_id={geoname_id} />
+      <AirportMap latitude={latitude} longitude={longitude} />
     </div>
   );
 }
